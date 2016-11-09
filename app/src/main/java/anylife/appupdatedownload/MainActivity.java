aa1ff6e5f049ccb0edd2ac10cd28e4a575646d68
B5FCH07.曾刘保 <zenglb@vanke.net.cn>
2016-11-09 15:30:31 +0800

    init

---
+++ app/src/main/java/anylife/appupdatedownload/MainActivity.java
@@ -0,0 +1,17 @@
+package anylife.appupdatedownload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * use retrofit2 to downLoad app from ds
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
